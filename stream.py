ebfca931a26496d423e4fe12ff9ce5c3bdd0dfb2
Mufeng Tang （唐 沐丰） <tangmufeng424@gmail.com>
2019-02-24 22:29:49 +0000

    Update and rename stream_test.py to stream.py

--- stream.py
+++ stream.py
@@ -1,3 +1,6 @@
+"""A simple programme for collecting data from the Twitter Streaming API
+More imformation on https://developer.twitter.com/en/docs.html"""
+
 from tweepy.streaming import StreamListener
 from tweepy import OAuthHandler
 from tweepy import Stream
@@ -9,31 +12,29 @@ access_token_secret = "your_access_token_secret"
 API_key = "your_API_key"
 API_secret_key = "your_API_secret_key"
 
-"""traffic_words = ['traffic jam', 'traffic', 'congest', 'congestion', 'congestion charge', 'jammed',
-                 'gridlock', 'rush hour', 'transport', 'bottleneck']"""
-traffic_words = ['traffic']
+#Keywords you wanna search
+keywords = ['Trump', 'Brexit']
 
 
 class StdOutListener(StreamListener):
 
     def on_data(self, data):
-        #print (data)
-        if any(x in json.loads(data)['text'].lower() for x in traffic_words):
+        if any(x in json.loads(data)['text'].lower() for x in keywords):
             print (data)
-            with open('streamdata1.txt', 'a') as file:
+            with open('data.txt', 'a') as file:
                 file.write(data+'\n')
         return True
 
     def on_error(self, status):
         print (status)
 
-
-#if __name__ == '__main__':
+        
 l = StdOutListener()
 auth = OAuthHandler(API_key, API_secret_key)
 auth.set_access_token(access_token, access_token_secret)
 stream = Stream(auth, l)
 
-stream.filter(follow=['47319664', '355994177'])
+#There are different parameters of filter; however, they can only be linked by "OR" conditions
+stream.filter(keywords=keywords)
 
 file.close()
